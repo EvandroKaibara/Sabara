af0a3da041c93c1da02fd9715c436f50ca8afe9d
EvandroKaibara <yujievandro@gmail.com>
2025-04-21 00:38:28 -0300

    H1 arrumando

--- src/componentes/FormLogin/index.jsx
+++ src/componentes/FormLogin/index.jsx
@@ -5,7 +5,7 @@ const FormLogin = () => {
     <>
       <div className='container-form-login'>
         <form className='form-login'>
-          <h1>Bem-vindo(a) ao portal médico. Por favor, insira suas credenciais para acessar o sistema.</h1>
+          <h1 className='h1-login'>Bem-vindo(a) ao portal médico. Por favor, insira suas credenciais para acessar o sistema.</h1>
           <div className='label-login'>
             <label>Login</label>
             <input type="text"/>

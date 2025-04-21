4016621d0a18b8c5348a2849322c9e9f487a5a7b
EvandroKaibara <yujievandro@gmail.com>
2025-04-21 00:45:37 -0300

    Corrigindo css

--- src/componentes/Banner/index.jsx
+++ src/componentes/Banner/index.jsx
@@ -3,8 +3,8 @@ import './Banner.css';
 const Banner = () => {
     return(
         <div className='container'>
-            <h1>OLÁ, DOUTOR(A)!</h1>
-            <h2>O que gostaria de ver hoje?</h2>
+            <h1 className='h1-banner'>OLÁ, DOUTOR(A)!</h1>
+            <h2 className='h2-banner'>O que gostaria de ver hoje?</h2>
         </div>
         
     );

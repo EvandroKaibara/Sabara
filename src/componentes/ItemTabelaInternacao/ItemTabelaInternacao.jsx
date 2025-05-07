1ff98027f4f301823ee461a557e3679acd440404
Evandro Kaibara <yujievandro@gmail.com>
2025-05-07 19:52:30 -0300

    Toaster Internacao

--- src/componentes/ItemTabelaInternacao/ItemTabelaInternacao.jsx
+++ src/componentes/ItemTabelaInternacao/ItemTabelaInternacao.jsx
@@ -1,10 +1,12 @@
 import './ItemTabelaInternacao.css';
 
+import { Toaster, toast } from 'sonner';
+
 const ItemTabela = ({ nome, atualizacao, estado }) => {
   return (
     <div className='container-linha'>
         <div className='linha-tabela linha-nome'>{nome}</div>
-        <div className='linha-tabela linha-prontuario'>Exibir prontuário</div>
+        <div className='linha-tabela linha-prontuario' onClick={() => toast.error('Erro ao comunicar com o servidor!')}><Toaster richColors/>Exibir prontuário</div>
         <div className='linha-tabela linha-data'>{atualizacao}</div>
         <div className='linha-tabela linha-estado'>{estado}</div>
     </div>

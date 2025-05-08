576853aa05630344493c19b4aed3f5cfce48ec7a
Evandro Kaibara <yujievandro@gmail.com>
2025-05-07 21:04:27 -0300

    Sooner

--- src/componentes/ItemTabelaInternacao/ItemTabelaInternacao.jsx
+++ src/componentes/ItemTabelaInternacao/ItemTabelaInternacao.jsx
@@ -6,7 +6,7 @@ const ItemTabela = ({ nome, atualizacao, estado }) => {
   return (
     <div className='container-linha'>
         <div className='linha-tabela linha-nome'>{nome}</div>
-        <div className='linha-tabela linha-prontuario' onClick={() => toast.alert('Função em construção!')}><Toaster richColors/>Exibir prontuário</div>
+        <div className='linha-tabela linha-prontuario' onClick={() => toast.warning('Função em construção!')}><Toaster richColors/>Exibir prontuário</div>
         <div className='linha-tabela linha-data'>{atualizacao}</div>
         <div className='linha-tabela linha-estado'>{estado}</div>
     </div>
